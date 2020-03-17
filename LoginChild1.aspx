b3d41b6c04d0a6e6946d3abec7cb7ec05ccbb4f4
PRIYANKA B <bpriyankauma1997@gmail.com>
2020-03-17 16:06:47 +0530

    Add project files.

---
+++ LoginChild1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LoginMaster.Master" AutoEventWireup="true" CodeBehind="LoginChild1.aspx.cs" Inherits="ASPLogin.LoginChild1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <table>
+        <tr>
+            <td>Empid</td>
+            <td>
+                <asp:TextBox ID="txtEmpid" runat="server"></asp:TextBox></td>
+            <td>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Empid is required" ControlToValidate="txtEmpid"></asp:RequiredFieldValidator></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td>
+                <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox></td>
+            <td>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Enter Password" ControlToValidate="txtPassword"></asp:RequiredFieldValidator></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" /></td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ LoginChild1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ASPLogin
+{
+    public partial class LoginChild1 : System.Web.UI.Page
+    {
+        SqlConnection Con = null;
+        SqlDataAdapter Adp = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Con = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlCon"].ToString());
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Adp = new SqlDataAdapter("select * from tbl_Login2 where Empid=@Eid and Password=@P",Con);
+            Adp.SelectCommand.Parameters.AddWithValue("@Eid", int.Parse(txtEmpid.Text));
+            Adp.SelectCommand.Parameters.AddWithValue("@P", txtPassword.Text);
+            DataSet Ds = new DataSet();
+            Adp.Fill(Ds, "E");
+            string name= "";
+            string type= "";
+            if(Ds.Tables["E"].Rows.Count==1)
+            {
+                name = Ds.Tables["E"].Rows[0][1].ToString();
+                type = Ds.Tables["E"].Rows[0][3].ToString();
+            }
+            if (type == "Admin")
+            {
+                Response.Cookies["name"].Value = name;
+                Response.Redirect("Admin.aspx");
+            }
+            else if (type == "Associate")
+            {
+                Response.Cookies["name"].Value = name;
+                Response.Redirect("Associate.aspx");
+            }
+            else
+                
+                Response.Redirect("LoginChild1.aspx");
+        }
+    }
+}
